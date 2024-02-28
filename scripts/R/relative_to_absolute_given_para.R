aefb48a6e3c1b3e1cc33a8f9e9cb85209bde21f5
Leanne Wu <wu_leanne@163.com>
2024-02-28 22:35:59 +0000

    correct the segments script

--- scripts/R/relative_to_absolute_given_para.R
+++ scripts/R/relative_to_absolute_given_para.R
@@ -3,7 +3,7 @@ library(tidyverse)
 
 input_dir <- '/path/to/_output.csv/'
 
-#input_dir <- '/scratchb/stlab-icgc/users/wu04/project/sWGS/results/qdnaseq/sarah_cohort/train/'
+#input_dir <- "C:/Users/lw748/OneDrive - University of Cambridge/sWGS/QDNASeq_and_model_training/RASCAL/github_rascal/results/SLX-10722_SLX-11823.D701_D504"
 
 # Set the ploidy and cellularity values
 # ploidy_list <- seq(1, 4, by=0.5)
@@ -13,6 +13,29 @@ cellularity_list <- seq(0.05, 0.15, by=0.05)
 
 # List all _output.csv files in the input directory
 file_list <- list.files(input_dir, pattern = "_output\\.csv$", full.names = TRUE, recursive = TRUE)
+
+# Define the function to process results in segments
+copy_number_segments_modified_from_rascal <- function (copy_number) 
+{
+  stopifnot(is.data.frame(copy_number))
+  stopifnot("sample" %in% names(copy_number))
+  stopifnot("chromosome" %in% names(copy_number))
+  stopifnot("start" %in% names(copy_number), is.numeric(copy_number$start))
+  stopifnot("end" %in% names(copy_number), is.numeric(copy_number$end))
+  stopifnot("segmented" %in% names(copy_number), is.numeric(copy_number$segmented))
+  copy_number %>% filter(!is.na(segmented)) %>% mutate(length = end - 
+                                                         start + 1) %>% arrange(ploidy, cellularity, sample, chromosome, start) %>% 
+    mutate(new_segment = row_number() == 1 | !(sample == 
+                                                 lag(sample) & chromosome == lag(chromosome) & segmented == 
+                                                 lag(segmented) & ploidy == lag(ploidy) & cellularity == 
+                                                 lag(cellularity))) %>% mutate(segment = cumsum(new_segment)) %>% 
+    group_by(segment) %>% summarize(sample = first(sample), 
+                                    chromosome = first(chromosome), start = first(start), 
+                                    end = last(end), copy_number = first(segmented), absolute_copy_number = first(absolute_segmented), bin_count = n(), 
+                                    sum_of_bin_lengths = sum(length), weight = sum(length)/median(length),
+                                    ploidy = first(ploidy), cellularity = first(cellularity))
+}
+
 
 # Define the function to convert relative CNA to absolute using given ploidy and cellularity values
 relative_to_absolute_given_para <- function(input_file, ploidy_list, cellularity_list){
@@ -38,7 +61,7 @@ relative_to_absolute_given_para <- function(input_file, ploidy_list, cellularity
   output_path_copy_number <- paste0(dirname(input_file), '/absolute_', basename(input_file))
   write.csv(results_df, output_path_copy_number, row.names = FALSE)
   #Save the results in segments
-  results_df_segments <- copy_number_segments(results_df)
+  results_df_segments <- copy_number_segments_modified_from_rascal(results_df)
   output_path_segmented <- paste0(dirname(input_file), '/absolute_segments_', basename(input_file))
   write.csv(results_df_segments, output_path_segmented, row.names = FALSE)
 }
